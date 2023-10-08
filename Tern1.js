fa1cccb0e8e10be73517fc30a1b7de0714de1294
alenakkkk <146487937+alenakkkk@users.noreply.github.com>
2023-10-09 01:22:05 +0400

    Tern1.js

--- Tern1.js
+++ Tern1.js
@@ -1,33 +1,26 @@
 let a = Math.floor(Math.random() * 100);
+let result;
 
-switch (true) {
-  case (a > 10):
-    if ((a > 10 ? a : a * 2) > 5) {
-      console.log((2 * a) + 1);
-    } 
-    else {
-      if ((a < 3 ? 1 : 2 * (a - 2)) > 4) {
-        console.log(5);
-      } 
-      else {
-        if (a % 2 == 0) {
-          console.log(6);
-        } 
-        else {
-          console.log(7);
-        }
-      }
-    }
-    break;
-  default:
-    if ((a < 3 ? 1 : 2 * (a - 2)) > 4) {
-      console.log(5);
-    } 
-    else {
-      if (a % 2 == 0) {
-        console.log(6);
-      } else {
-        console.log(7);
-      }
-    }
+if (a > 10) {
+  result = a;
+} else {
+  result = a * 2;
+}
+
+if (result > 5) {
+  console.log(2 * a + 1);
+} else {
+  let innerResult;
+
+  if (a < 3) {
+    innerResult = 1;
+  } else {
+    innerResult = 2 * (a - 2);
+  }
+
+  if (innerResult > 4) {
+    console.log(5);
+  } else {
+    console.log(a % 2 === 0 ? 6 : 7);
+  }
 }
